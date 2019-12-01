4a4ba92aac73127a3be67aea689f0448b40848da
Ágoston Reischl <ragoston001@gmail.com>
2019-12-01 14:24:42 +0100

    second

--- version_control_feladat/version_control_feladat/Form1.cs
+++ version_control_feladat/version_control_feladat/Form1.cs
@@ -7,17 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using version_control_feladat.entities;
 
 namespace version_control_feladat
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+            var u = new User()
+            {
+                LasName = textBox1.Text, FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
